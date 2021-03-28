44831aa864bc3bc8d064987dab10b756d24a4ec4
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-03-28 02:23:01 -0400

    create conditional console logs for time tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 console.log("check");
 
+//create varaible to hour format//
+var hourFormat = "hA";
+
 // variable to get local date and time//
 
 var date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
@@ -10,34 +13,32 @@ var localDate  = moment.utc(date).toDate();
 localTime = moment(localTime).format('hA');
 localDate = moment(localDate).format('dddd, MMMM Do');
 
-
-
 console.log("moment: " + localTime);
 console.log("moment: " + localDate);
+
 
 // add local date to HTML//
 var currentDayEl = document.getElementById('currentDay');
 
 currentDayEl.innerText = localDate;
 
-
-// function to change time textarea colour according to local time//
+// find which row has is greater than, equal to or less than local time//
 var textAreaEl = document.getElementById("#textarea");
 
 var hourEls = document.getElementsByClassName("hour");
 for (var i = 0; i < hourEls.length; i++) {
     var hour = hourEls[i].innerText;
     console.log("Hours: " + hour);
-};
 
-function textAreaColourChange() {
-    console.log("text area colour change begining");
-    if(hour <= localTime) {
-        textAreaEl.classList.add('past');
+    var convertedTime = moment(hour, hourFormat);
+    console.log(convertedTime.diff(moment(), 'hours'));
+    if(convertedTime.diff(moment().startOf('hour'), 'hours') > 0) {
+        console.log("true");
+    }else if(convertedTime.diff(moment().startOf('hour'), 'hours') < 0) {
+        console.log("false");
+    }else {
+        console.log("same");
     };
-
-
 };
 
-textAreaColourChange();
 
